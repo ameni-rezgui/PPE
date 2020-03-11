1064986ede8ffbdb69bdc84d69f7d7ac43eed280
Ameni <ameni.rezgui@sesame.com.tn>
2020-03-11 16:48:42 +0100

    First commit

---
+++ PPEVisiteTech/src/main/java/com/example/demo/PpeVisiteTechApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpeVisiteTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpeVisiteTechApplication.class, args);
+	}
+
+}
